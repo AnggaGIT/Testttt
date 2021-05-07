e209f106e844215f65a260a24cd2bdbc092184e3
Lisa2404 <anggapb2404@gmail.com>
2021-05-07 15:15:01 +0700

    prak
    adadwadawd

---
+++ Minggu7/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Oracle.DataAccess.Client;
+
+
+namespace Minggu7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        OracleConnection conn;
+        string connectionString;
+        Window1 w1;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (textDS.Text == "" || textUsername.Text == "" || textPassword.Text == "")
+            {
+                MessageBox.Show("Terdapat field yang belum diisi");
+            }
+            else
+            {
+                connectionString = "Data Source = " + textDS.Text + "; User ID=" + textUsername.Text + "; Password=" + textPassword.Text;
+                conn = new OracleConnection(connectionString);
+                try
+                {
+                    conn.Open();
+                    w1 = new Window1(conn);
+                    w1.ShowDialog();
+                    conn.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Terdapat input yang salah");
+                    conn.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Minggu7/Window1.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Oracle.DataAccess.Client;
+
+namespace Minggu7
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        OracleConnection conn;
+        public Window1(OracleConnection con)
+        {
+            InitializeComponent();
+            conn = con;
+        }
+
+        private void BtnCari_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnCari_Click_1(object sender, RoutedEventArgs e)
+        {
+            if(txtCari.Text == "")
+            {
+                MessageBox.Show("Nama tidak boleh kosong");
+            }
+            else
+            {
+                CrystalReport1 rpt = new CrystalReport1();// inisialisasi Report kalian 
+                rpt.SetParameterValue("search", txtCari.Text);// ini buat lempar parameter ke report
+                reportViewer.ViewerCore.ReportSource = rpt;// pasang report ke viewer
+            }
+            
+        }
+    }
+}
